8e224fa48f2b0522ffcfd94cf94c536ca276e38e
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-12-03 01:42:22 +0000

    deletion

---
+++ Array/Deletion_in_Array.c
@@ -0,0 +1,36 @@
+// Delete an element from an array for the above similar cases :
+
+#include <stdio.h>
+int main(){
+    int array[10]={32,42,56,68,59,78,83,95,67,25};
+    int choice,pos;
+    for(int i=0;i<10;i++){
+        printf("%d ",array[i]);
+    }
+    printf("Enter the choice as :\n1 -> from First position\n2 -> from Last position\n3 -> from any postion\n");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+          for(int i=1;i<10;i++){
+            array[i-1]=array[i];
+          }
+          break;
+        case 2:
+          break;
+        case 3:
+          printf("Enter the position of element to be deleted : ");
+          scanf("%d",&pos);
+          for(int i=pos;i<10;i++){
+            array[i]=array[i+1];
+          }
+          break;
+        default :
+          printf("Invalid choice.");
+          return 0;
+               
+    }
+    for(int i=0;i<9;i++){
+        printf("%d ",array[i]);
+    }
+
+}
