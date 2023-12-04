b6bf752f21c9a677d82c26a5e0cb675a76789aae
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-12-04 03:31:14 +0000

    creation of  linked list

---
+++ Linked list/creation.c
@@ -0,0 +1,38 @@
+// creation of linked list...
+
+#include <stdio.h>
+#include <malloc.h>
+//Self referential structure
+struct node{
+    int info;
+    struct node *link;
+};
+struct node *start=NULL;
+
+int main(){
+    struct node *new ,*present,*i;
+    int t=11;
+    printf("Remember terminating value is 0\n\n");
+    while(t!=0){
+        new=(struct node *)malloc(sizeof(struct node));
+        printf("Enter the information : ");
+        scanf("%d",&t);
+        new->info=t;
+        new->link=NULL;
+        if(start==NULL){
+            start=present=new;
+        }
+        else{
+            present->link=new;
+            present=new;
+        }
+
+    }
+
+    //Traversial in linked list...
+    for(i=start;i->link!=NULL;i=i->link){
+        printf("%d --> ",i->info);
+    }
+    printf("%d",i->info); 
+    return 0;
+}
